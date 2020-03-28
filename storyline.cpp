c8664e7020383c80880b0e616b06bb502fb0a9fc
alfab3 <alfabrizi3@gmail.com>
2020-03-28 12:34:11 -0400

    first commit

---
+++ storyline.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+  std::cout << "Welcome to Othea\n";
+  int init_bg;
+  std::cout << "Please select your backrgound:\n";
+  std::cout << "1: Traveler stopped at an Inn\n";
+  std::cout << "2: An apprentice of a trade\n";
+  std::cout << "3: A slave\n";
+  std::cin >> init_bg;
+
+
+}
